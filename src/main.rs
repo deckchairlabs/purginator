638b1491dc78cb3e1fdf4868be009bd5d9fddaa3
James Edmonds <james@deckchairlabs.com>
2022-01-14 04:00:39 +0000

    reworked so any true match breaks the loop

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
 use cssparser::ToCss;
-use parcel_css::rules::CssRule::{self, Style};
+use parcel_css::rules::CssRule::Style;
 use parcel_css::stylesheet::{ParserOptions, StyleSheet};
 use scraper::{Html, Selector};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let html = r#"
-    <!DOCTYPE html>
+        <!DOCTYPE html>
         <html lang="en>
             <head>
                 <meta charset="UTF-8">
@@ -16,8 +16,9 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                 <div class="foo">
                     This is foo
                 </div>
-                <h1 class="unused">
-                    This is unused
+                
+                <h1 class="undefined">
+                    This is undefined
                 </h1>
                 
                 <section id="main" class="multiple selectors">
@@ -58,48 +59,50 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     .to_string();
 
     let parsed = StyleSheet::parse(
-        "style.css".to_string(),
+        "styles.css".to_string(),
         &css,
         ParserOptions {
-            nesting: true,
+            nesting: false,
             css_modules: false,
         },
     );
 
-    let mut rules: Vec<CssRule> = Vec::new();
+    if let Ok(stylesheet) = parsed {
+        let document: Html = Html::parse_document(&html);
+        let select_all: Selector = Selector::parse("*").unwrap();
 
-    if let Ok(mut stylesheet) = parsed {
-        rules.append(&mut stylesheet.rules.0);
-    } else {
-        panic!("Parsing failed")
-    }
+        for (index, css_rule) in stylesheet.rules.0.iter().enumerate() {
+            let mut matched = false;
 
-    let document: Html = Html::parse_document(&html);
-    let select_all: Selector = Selector::parse("*").unwrap();
+            if let Style(rule) = css_rule {
+                let all_elements = document.select(&select_all);
 
-    let all_elements = document.select(&select_all);
+                for element_ref in all_elements {
+                    let mut selector_string = String::new();
+                    let iter = rule.selectors.0.iter().clone().rev();
 
-    for element_ref in all_elements {
-        for rule in rules.iter() {
-            if let Style(rule) = rule {
-                let element_name = element_ref.value().name();
+                    for component in iter {
+                        component.to_css(&mut selector_string)?
+                    }
 
-                let mut selector_string = String::new();
-                let iter = rule.selectors.0.iter().clone().rev();
+                    let selector = Selector::parse(&selector_string).unwrap();
 
-                for component in iter {
-                    component.to_css(&mut selector_string)?
-                }
+                    matched = selector.matches(&element_ref);
 
-                let selector = Selector::parse(&selector_string).unwrap();
-
-                if selector.matches(&element_ref) {
-                    println!("{} Matches! {}", element_name, selector_string);
-                } else {
-                    println!("{} Doesn't match! {}", element_name, selector_string);
+                    if matched {
+                        break;
+                    }
                 }
             }
+
+            if !matched {
+                // How can we remove the rule at this index
+                // stylesheet.rules.0.remove(index);
+                dbg!(index);
+            }
         }
+    } else {
+        panic!("Parsing failed")
     }
 
     Ok(())
